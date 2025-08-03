ca1a86d4f69fcb5c00c66adba436b26c8d9f7d87
Dmytro Kubatko <d.kubatko@rozetkapay.com>
2025-08-03 14:10:27 +0300

    reduce font size

--- font.lua
+++ font.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font_size = 20
+config.font_size = 18
 config.line_height = 1.2
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font = wezterm.font("Liga SFMono Nerd Font")
